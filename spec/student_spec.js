2392deb0197b0220c13491f65f0169ce8c47d273
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-03 21:57:44 +0800

    modify student_spec.js

--- spec/student_spec.js
+++ spec/student_spec.js
@@ -3,8 +3,12 @@ var Student = require("../src/student.js");
 
 describe("Person", function(){
 
+    var student;
+    beforeEach(function () {
+        student = new Student('Tom', 21, 2);
+    });
+
     it("should have name age and class", function () {
-        var student = new Student("Tom", 21,2);
         expect(student.age).toEqual(21);
         expect(student.name).toEqual("Tom");
         expect(student.classs).toEqual(2);
@@ -12,7 +16,6 @@ describe("Person", function(){
 
     describe("#introduce()", function() {
         it("should return right", function () {
-            var student = new Student("Tom", 21,2);
             expect(student.introduce()).toEqual('I am a Student, I am at Class 2.');
         });
     });
